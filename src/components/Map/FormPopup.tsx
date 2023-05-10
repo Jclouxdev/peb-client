c227f47b917459c9257b05e9df682275b07d12c4
Jclouxdev <joseph.clx@gmail.com>
2023-05-10 11:39:13 +0200

    SearchBar Inprogress

--- src/components/Map/FormPopup.tsx
+++ src/components/Map/FormPopup.tsx
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import "./Form.css"
-import ICategorie from '../../pages/App/ICategorie';
+import React, { useEffect, useState } from "react";
+import "./Form.css";
+import ICategorie from "../../pages/App/ICategorie";
 import * as yup from "yup";
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 interface PropTypes {
   onSubmit: (data: any) => Promise<void>;
   latPosition: {};
   lngPosition: {};
-
 }
 
 const schema = yup.object().shape({
@@ -17,59 +16,61 @@ const schema = yup.object().shape({
   description: yup.string().max(40).required(),
   categorieId: yup.number().required(),
   lat: yup.number().required(),
-  lon: yup.number().required()
+  lon: yup.number().required(),
 });
 
-export function ModalPopup({onSubmit, latPosition, lngPosition}: PropTypes) {
-    const [categories, setCategories] = useState<ICategorie[] | undefined>();
+export function ModalPopup({ onSubmit, latPosition, lngPosition }: PropTypes) {
+  const [categories, setCategories] = useState<ICategorie[] | undefined>();
 
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm({
-      resolver: yupResolver(schema),
-      mode: "onSubmit",
-      defaultValues: {
-        lat: latPosition,
-        lon: lngPosition,
-        name: "",
-        description: "",
-        categorieId: 1,
-      },
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onSubmit",
+    defaultValues: {
+      lat: latPosition,
+      lon: lngPosition,
+      name: "",
+      description: "",
+      categorieId: 1,
+    },
+  });
 
-    useEffect(() => {
-      fetch(`http://localhost:8080/categories`, {
-        method: "GET",
-      })
-        .then((response) => response.json())
-        .then((data: ICategorie[]) => {
-          setCategories(data);
-        });
-    }, []);
-  
-    const onSubmitHandler = async (data) => {
-      try {
-          const token = localStorage.getItem("token");
-          const response = await fetch("http://localhost:8080/markers/create", {
-              method: "POST",
-              headers: { "Content-Type": "application/json", 'Access-Control-Allow-Origin': '*', Authorization: `Bearer ${token}` },
-              body: JSON.stringify(data),
-          });
-          const responseData = await response.json();
-          console.log(responseData);
-      } catch (error) {
-          console.error(error);
-      }
+  useEffect(() => {
+    fetch(`http://localhost:8080/categories`, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data: ICategorie[]) => {
+        setCategories(data);
+      });
+  }, []);
+
+  const onSubmitHandler = async (data: any) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetch("http://localhost:8080/markers/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+      });
+      const responseData = await response.json();
+      console.log(responseData);
+    } catch (error) {
+      console.error(error);
     }
-    
+  };
 
   return (
     <div className="popup-wrapper">
-      <form className='login-box' onSubmit={handleSubmit(onSubmitHandler)}>
-        <div>
-        </div>
+      <form className="login-box" onSubmit={handleSubmit(onSubmitHandler)}>
+        <div></div>
         <div className="user-box">
           <input
             type="text"
@@ -87,24 +88,17 @@ export function ModalPopup({onSubmit, latPosition, lngPosition}: PropTypes) {
           />
         </div>
         <div className="user-box">
-          <select
-            {...register("categorieId")}>
-              <>
-                {
-                  categories?.map(element => {
-                    return <option value={element.id}>{element.name}</option>
-                  })
-                }
-              </>
+          <select {...register("categorieId")}>
+            <>
+              {categories?.map((element) => {
+                return <option value={element.id}>{element.name}</option>;
+              })}
+            </>
           </select>
-          {errors.categorieId && 
-            <p>{errors.categorieId.message}</p>
-          }
+          {errors.categorieId && <p>{errors.categorieId.message}</p>}
         </div>
         <div>
-          <button
-          type="submit"
-          >Submit</button>
+          <button type="submit">Submit</button>
         </div>
       </form>
     </div>
